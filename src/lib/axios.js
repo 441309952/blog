17d12b7493e8c3b2ee1bef509fce829f240b4483
linzewei <linzewei@huishoubao.com.cn>
2020-01-09 00:22:27 +0800

    测试

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -3,7 +3,7 @@ import { message } from 'antd'
 // import NProgress from 'nprogress'
 
 const instance = axios.create({
-  baseURL: process.env.NODE_ENV === 'development' ? 'http://127.0.0.1:6060' : '', // api的base_url
+  baseURL: 'http://47.106.253.93:6060',
   timeout: 20000 // 请求超时时间
 })
 
